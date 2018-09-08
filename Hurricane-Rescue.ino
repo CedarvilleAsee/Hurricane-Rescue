dd170b440bca92f74bc0a24e946a08cb7112d971
Ian Steptoe <mathcrazy3.14159@gmail.com>
2018-09-08 16:39:38 -0400

    Beginning to translate code

--- Hurricane-Rescue.ino
+++ Hurricane-Rescue.ino
@@ -3,29 +3,29 @@
 #include "sorter.h"
 #include "new_pins.h"
 #include "constants.h"
+#include "missions.h"
 
-// Already defined in sorter.cpp
-/*bool delayState(int ms) {
-  static int milliseconds = -1;
-  if(milliseconds == -1) {
-    milliseconds = millis();
-  }
-  else if(millis() - milliseconds >= ms) {
-    milliseconds = -1;
-    return true;
-  }
-  return false;
-}*/
+
+
 
 int sensors[8]     = {0};
 int firstLineIndex = -1;
 int lastLineIndex  = -1;
 int amountSeen     = 0;
-int printVar = 0;
-bool otherPrintVar = false;
 
-// For bluetooth sender
-int iterations = 0;
+bool atWall = false;
+bool turning = false;
+
+char redPath[40];
+char neutralPath[40];
+char redPickup[40];
+char neutralPickup[40];
+int redSteps;
+int neutralSteps;
+int missionNum = 1;
+
+int redIndex = 0;
+int neutralIndex = 0;
 
 void setup() {
 
@@ -94,23 +94,45 @@ void readLine() {
   }
 }
 
-void writeWheelDirection(bool dir) {
-  if(dir) {
-    digitalWrite(WHEEL_DIR_LB, LOW );
-    digitalWrite(WHEEL_DIR_LF, HIGH);
-    digitalWrite(WHEEL_DIR_RF, HIGH);
-    digitalWrite(WHEEL_DIR_RB, LOW );
-  } else {
-    digitalWrite(WHEEL_DIR_LB, HIGH);
-    digitalWrite(WHEEL_DIR_LF, LOW);
-    digitalWrite(WHEEL_DIR_RF, LOW);
-    digitalWrite(WHEEL_DIR_RB, HIGH);
-  }
+int readFrontRight(){//gets data val from right infraread sensor IMH
+  return analogRead(FRONT_RIGHT_SENSOR);
+}
+int readFrontLeft(){//gets data val from left infraread sensor IMH
+  return analogRead(FRONT_LEFT_SENSOR);
+}
+bool twoConsecutiveAtMiddle() {
+  return twoConsecutive() && firstLineIndex >= TARGET_INDEX;
 }
 
+
+bool twoConsecutive() {
+  int lowCount = 0;
+  bool consecutive = false;
+  for(int i = 0; i < 7; i++) {
+    if(sensors[i] == HIGH && sensors[i + 1] == HIGH) {
+      consecutive = true;
+    }
+
+    if(sensors[i] == LOW) {
+      lowCount++;
+    }
+  }
+  if(sensors[7] == LOW) {
+    lowCount++;
+  }
+  return lowCount == 6 && consecutive;
+}
+void writeWheelDirection(bool ldir, bool rdir) {
+  digitalWrite(WHEEL_DIR_LF, ldir);
+  digitalWrite(WHEEL_DIR_LB, !ldir);
+  digitalWrite(WHEEL_DIR_RF, rdir);
+  digitalWrite(WHEEL_DIR_RB, !rdir);
+}
+
+
 void writeToWheels(int ls, int rs) {
-  analogWrite(WHEEL_SPEED_L, ls);
-  analogWrite(WHEEL_SPEED_R, rs);
+  analogWrite(WHEEL_SPEED_L, abs(ls));
+  analogWrite(WHEEL_SPEED_R, abs(rs));
 }
 
 bool lineFollow(int ts, int strictness) {
@@ -119,32 +141,85 @@ bool lineFollow(int ts, int strictness) {
   int leftSpeed = ts + offset*strictness;
   writeToWheels(leftSpeed, rightSpeed);
 
-  //return true if line follow state is done
-  return amountSeen > TURN_AMOUNT;
+  // Return true if the sensors can see a fork
+  if(readFrontRight < 30) {
+    atWall = true;
+    return true;
+  }
+  return (digitalRead(FORK_SENSOR) == HIGH)//amountSeen > TURN_AMOUNT;
 }
 
-bool turn(int ts, int strictness, char direction) {
-  if(direction == LEFT){
-    writeToWheels(ts - strictness, ts + strictness);
-  }else{
-    writeToWheels(ts + strictness, ts - strictness);
+bool turn(int spd, char dir) {
+  static int lineCount = 0;
+  if(dir == LEFT){
+    writeToWheels(-spd, spd);
+  }else if(dir == RIGHT){
+    writeToWheels(spd, -spd);
+  }else if(dir == FORWARD) {
+    writeToWheels(spd, spd);
   }
 
-  // FIXME: need to check "amountSeen < 3" this is an arbitrary value dependant on the type of turn
-  return firstLineIndex >= TARGET_INDEX  && amountSeen < 3;
+  if(dir == BACK && !atWall) {
+    if(twoConsecutiveAtMiddle()) { // if it isn't at a wall, the line sensors have to pass another line to turn completely around
+      lineCount++;
+    }
+    if(lineCount == 2){
+      lineCount = 0;
+      return true;
+    }
+  }
+  // Return true if the robot is back centered on the line
+  else return twoConsecutiveAtMiddle();
 }
 
-// Old robot code we probably don't need
-bool findLine(int ts) {
-  writeToWheels(ts, ts);
-  return amountSeen >= 2;
+bool delayState(int ms) {
+  static int milliseconds = -1;
+  if(milliseconds == -1) {
+    milliseconds = millis();
+  }
+  else if(millis() - milliseconds >= ms) {
+    milliseconds = -1;
+    return true;
+  }
+  return false;
 }
-
 // State 0
 //  Waits until the button on board is pushed. When it is pushed then go to next state
 bool waitState() {
   writeToWheels(0, 0);
   if(digitalRead(BUTTON1) == LOW) {
+    if(mission == 1) {
+      strcpy(redPath, RED_MISSION_1);
+      strcpy(neutralPath, NEUTRAL_MISSION_1);
+      strcpy(redPickup, RED_PICKUP_1);
+      strcpy(neutralPickup, NEUTRAL_PICKUP_1);
+      redSteps = RED_STEPS_1;
+      neutralSteps = NEUTRAL_STEPS_1;
+    }
+    else if(mission == 2) {
+      strcpy(redPath, RED_MISSION_2);
+      strcpy(neutralPath, NEUTRAL_MISSION_2);
+      strcpy(redPickup, RED_PICKUP_2);
+      strcpy(neutralPickup, NEUTRAL_PICKUP_2);
+      redSteps = RED_STEPS_2;
+      neutralSteps = NEUTRAL_STEPS_2;
+    }
+    else if(mission == 3) {
+      strcpy(redPath, RED_MISSION_3);
+      strcpy(neutralPath, NEUTRAL_MISSION_3);
+      strcpy(redPickup, RED_PICKUP_3);
+      strcpy(neutralPickup, NEUTRAL_PICKUP_3);
+      redSteps = RED_STEPS_3;
+      neutralSteps = NEUTRAL_STEPS_3;
+    }
+    else if(mission == 4) {
+      strcpy(redPath, RED_MISSION_4);
+      strcpy(neutralPath, NEUTRAL_MISSION_4);
+      strcpy(redPickup, RED_PICKUP_4);
+      strcpy(neutralPickup, NEUTRAL_PICKUP_4);
+      redSteps = RED_STEPS_4;
+      neutralSteps = NEUTRAL_STEPS_4;
+    }
     return true;
   }
   return false;
@@ -185,41 +260,75 @@ bool followTrackState() {
   return isFinished;
 }
 
+
+
+bool doTurnSequence(const char sequence[], int index, int maxSteps) {
+  if(turning) {
+    if(turn(HALF_SPEED, sequence[index])) {
+      turning = false;
+      return true;
+    }
+  } else {
+    if(index + 1 < maxSteps){
+      if(sequence[index + 1] == FORWARD) {
+        turning = lineFollow(FULL_SPEED, 50);
+        return false;
+      }
+    }
+    turning = lineFollow(HALF_SPEED, 50);
+  }
+  return false;
+}
+
+bool followRedPathState() {
+  if(redIndex == redSteps) return true;
+  if(doTurnSequence(redPath, redIndex, redSteps) &&
+    doPickupSequence(redPickup, redIndex, redSteps)) redIndex++;
+
+  return false;
+}
+
+bool followNeutralPathState() {
+
+  if(neutralIndex == neutralSteps) return true;
+  if(doTurnSequence(neutralPath, neutralIndex, neutralSteps) &&
+    doPickupSequence(neutralPickup, neutralIndex, neutralSteps)) neutralIndex++;
+
+  return false;
+}
+
+bool depositPeopleState(){
+  dump.write(DO_DUMP);
+  if(delayState(2000)) {
+    dump.write(DONT_DUMP);
+    return true;
+  }
+  return false;
+}
+
 void loop() {
   static int state = 0;
   readLine();
-  switch(state) {
+  switch(state)
+  {
     case 0:
-      if(waitState())  state++;
+      if(pickMissionState())  state++;
       break;
     case 1:
-      if(followTrackState())  state++;
+      if(followRedPathState())  state++;
       break;
-    //case 2:
-      //if(Next state is done)  state++;
-      //break;
+    case 2:
+      if(depositPeopleState())  state++;
+      break;
+    case 3:
+      if(followNeutralPathState()) state++;
+      break;
     case 4:
-      //if(Last state is done) state = 0;
+      if(depositPeopleState()) state++;
+      break;d
+  default:
+      if(doneState()) state = 0;
       break;
   }
 
-  iterations++;
-  if(iterations == BLUETOOTH_LIMITER) {
-    iterations = 0;
-    Serial3.print("[ ");
-    for(int i = 0; i < 8; i++) {
-      Serial3.print(sensors[i]);
-      Serial3.print(" ");
-    }
-    Serial3.println("]");
-    //Serial3.print("[ ");
-    //Serial3.print(firstLineIndex);
-    //Serial3.print(", ");
-    //Serial3.print(amountSeen);
-    //Serial3.print(" ]");
-    //Serial3.print(" - ");
-    //Serial3.print(printVar);
-    //Serial3.print(", ");
-    //Serial3.println(otherPrintVar);
-  }
 }
